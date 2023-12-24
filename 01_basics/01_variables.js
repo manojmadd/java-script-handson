384c13a9190b28dc2ac03fd3e4567dc856899124
Manoj Maddheshiya <84504403+manojmadd@users.noreply.github.com>
2023-12-24 08:05:11 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 144553
+let accountEmail = "mmaddheshiya05@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur" /*  */
+let accountState;
+
+// accountId=2 // not allowed
+
+/* const ka matlab ye hota hai isko change nhi kiya jaa sakta hai */
+
+/*variable ko declarare karne ke do tarike js me ek issue tha isko nhi pta tha ki scope kya hai? scope jo aap curley bracket likhate ho usi ko scope bola jata hai scope ke basis pe kam hi nhi karti thi 
+we will use simply use let use karenge aur bhul jayiye var kya hota hai kyunki 
+prefer not to use var
+bcz of issue in block scope and functional */
+
+accountEmail = "hhhs@gmail.com"
+accountPassword = "54321"
+accountCity = "Bengluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict" // treat all JS code as newer version
+// alert(3+3)// we are using nodejs, not browser
+
+//code readability should be high //
+
+let name = "Manoj"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to the power 53
+//bigint= number agar bahut bada hai uske liye use karte hai
+//string => single quote me bhi use kar sakte hai aur double quote me bhi but prefer ""
+//boolean => simple yes or no true/false
+//null=> standalone value hai- ek representation hai empty value ka aap isko assign bhi kar sakte hai means wo khali hai//null ka type object hotya hai 
+//undefined => jab aapne value define nhi kri hai // iska datatype undefined hota hai
+//symbol=> react ke bare me symbol ka use karte hai important - unique ke liye
+
+// object=>
+
+console.log(typeof "Manoj");
