1f6f58004239f3e78b8d7f6d52877881a4d1d784
Manoj Maddheshiya <84504403+manojmadd@users.noreply.github.com>
2023-12-24 09:07:24 +0000

    Conversion operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+// let score = ""
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+
+// "33" =>33
+// "33abc" => NaN(Not a number)
+// true => 1; false =>0
+
+let isLoggedIn = "Manoj"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=> false
+//"" => false
+//"Manoj" => true
+
+let someNumber = 33
+
+let stringnumber = String(someNumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
