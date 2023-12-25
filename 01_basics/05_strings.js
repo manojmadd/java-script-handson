2df4c8ef7dddf2dc98e7f79040f0b659dd4d9e46
Manoj Maddheshiya <84504403+manojmadd@users.noreply.github.com>
2023-12-25 15:47:48 +0000

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,13 @@
+const name = "manoj"
+const lname = "maddhehsiya"
+
+// console.log(name + lname) 
+
+// string interpolation’s 
+console.log(`Hello My name is ${name} and my lname is ${lname}`);
+
+const gameName = new String('manoj')
+
+console.log(gameName[0])
+
+// method length, .toUpeerCase, .charAt, indexOf, subString
--- 01_basics/dataTypesSummary.js
+++ 01_basics/dataTypesSummary.js
@@ -1,23 +1,39 @@
-// # Type of data
+// // # Type of data
 
-// Primitive 
-// 7 types : String, Number, boolean, null, undefined,Symbol, BigInt- Call by value hote hai means jab aap bhi koi data copy karte hai to inka jo original data hai wo reference aapko memory ka nhi diya jata hai inko copy karke aapko diya jata hai usko copy karke diya jata hai usme aapka ho gya number string 
+// // Primitive 
+// // 7 types : String, Number, boolean, null, undefined,Symbol, BigInt- Call by value hote hai means jab aap bhi koi data copy karte hai to inka jo original data hai wo reference aapko memory ka nhi diya jata hai inko copy karke aapko diya jata hai usko copy karke diya jata hai usme aapka ho gya number string 
 
-// dynamically type hai ya statically type lang ? type of variable? const score = 100 ham java script ke andar lan ko defined  nhi karte hai
+// // dynamically type hai ya statically type lang ? type of variable? const score = 100 ham java script ke andar lan ko defined  nhi karte hai
 
-//Reference(Non primitive) ye sari values wo hai ki memeory me inka reference directly allocate kiya jaa sakta. hai 
-// Array, Objects, Functions
+// //Reference(Non primitive) ye sari values wo hai ki memeory me inka reference directly allocate kiya jaa sakta. hai 
+// // Array, Objects, Functions
 
-const heros = ["Iron man", "Robin"]
-let myObj=
-{
-    name: "manoj",
-    age:22,
+// const heros = ["Iron man", "Robin"]
+// let myObj=
+// {
+//     name: "manoj",
+//     age:22,
 
-}
+// }
 
-const myFunction= function(){
-    console.log("Hello world");
-}
+// const myFunction= function(){
+//     console.log("Hello world");
+// }
 
-console.log(typeof myFunction);
+// console.log(typeof myFunction);
+
+/// *************
+
+// Stack(Primitive ), Heap(Non-primitive)
+
+let myname = "Manoj"
+
+let anotherName =" Maddheshiya"
+
+anotherName = "Mau"
+
+console.log(myname);
+console.log(anotherName);
+
+
+
