982f458f2d70508820711098c8a002256ce5b180
Manoj Maddheshiya <84504403+manojmadd@users.noreply.github.com>
2023-12-26 17:46:45 +0000

    Date

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -10,4 +10,7 @@ const gameName = new String('manoj')
 
 console.log(gameName[0])
 
-// method length, .toUpeerCase, .charAt, indexOf, subString
+// method length, .toUpeerCase, .charAt, indexOf, subString(- nhi de sakte hai), slice ke andar aap negative value bhi use kar sakte hai,
+// trim(starting and ending space trim kar deta hai )
+// replace()
+// split()
---
+++ 01_basics/06_numbers_and_maths.js
@@ -0,0 +1,19 @@
+const score = 400
+
+const balance = new Number(1000)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));
+
+const othernummber = new Number(23.8966)
+// console.log(othernummber.toPrecision(3));
+
+const number = new Number(1000000)
+// console.log(number.toLocaleString('en-IN'));
+
+///***** Maths ******/
+
+console.log(Math)
+console.log(Math.abs(-4));
